1d72712f10ccdcf8caf7459bbaf1ca9f18db7d24
priyareddy21 <priyareddy21193@gmail.com>
2024-03-10 01:51:47 -0500

    Added authorization and authentication: to users, admin, public

--- src/main/java/com/example/demo/config/WebSecurityCls.java
+++ src/main/java/com/example/demo/config/WebSecurityCls.java
@@ -2,17 +2,40 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity(debug=true)
 public class WebSecurityCls {
 
+	
+
+	
+	@Bean
+	SecurityFilterChain setupfilterChain(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeHttpRequests((authorize) -> authorize
+			.requestMatchers("/api/user").hasRole("user")
+			.requestMatchers("/api/admin").hasRole("admin")
+			.requestMatchers("api/public").permitAll()
+			.anyRequest().authenticated());
+		http.formLogin(Customizer.withDefaults());
+		http.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+		
+	}
+	
+	
 	@Bean
 	public InMemoryUserDetailsManager userDetails(PasswordEncoder passwordEncoder) {
 		
@@ -34,6 +57,9 @@ public class WebSecurityCls {
 		
 		
 	}
+	
+	
+	
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
